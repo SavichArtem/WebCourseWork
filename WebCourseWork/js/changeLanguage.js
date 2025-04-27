0b9ef85024a9e28c0526eb4a8edb90f4ef16be2c
SavichArtem <mods_b@mail.ru>
2025-04-28 01:19:40 +0300

    feat: add language change

---
+++ WebCourseWork/js/changeLanguage.js
@@ -0,0 +1,84 @@
+const langButtons = document.querySelectorAll("[data-btn]");
+const allLangs = ['ru', 'en'];
+const currentPathName = window.location.pathname;
+let currentLang = localStorage.getItem('language') || checkBrowserLang() || 'en';
+let currentText = {};
+
+import { homePageText } from "./language_pages/languages_main-page.js";
+
+function checkPagePathName() {
+    switch (currentPathName) {
+        case '/main.html':
+            currentText = homePageText;
+            break;
+        case '/menu.html':
+            currentText = menuPageText;
+            break;
+        default:
+            currentText = homePageText;
+            break;
+    }
+}
+
+checkPagePathName();
+
+function changeLang() {
+    for (const key in currentText) {
+        const elem = document.querySelector(`[data-lang=${key}]`);
+        if (elem) {
+            elem.textContent = currentText[key][currentLang];
+        }
+    }
+}
+
+changeLang();
+
+langButtons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        currentLang = event.target.dataset.btn;
+        localStorage.setItem('language', event.target.dataset.btn)
+        resetActiveClass(langButtons, "header_btn_active")
+        btn.classList.add("header_btn_active");
+        changeLang();
+    });
+});
+
+function resetActiveClass(arr, activeClass){
+    arr.forEach(elem=>{
+        elem.classList.remove(activeClass)
+    })
+}
+
+function checkActiveLangButton() {
+    switch(currentLang){
+        case 'ru':
+            document
+                .querySelector('[data-btn="ru"]')
+                .classList.add("header_btn_active");
+            break;
+        case 'en':
+            document
+                .querySelector('[data-btn="en"')
+                .classList.add('header_btn_active');
+            break;
+        default:
+            document
+                .querySelector('[data-btn="ru"]')
+                .classList.add("header__btn_active");
+            break;
+    }
+}
+
+checkActiveLangButton();
+
+function checkBrowserLang() {
+	const navLang = navigator.language.slice(0, 2).toLowerCase();
+	const result = allLangs.some((elem) => {
+		return elem === navLang;
+	});
+	if (result) {
+		return navLang;
+	}
+}
+
+console.log("navigator.language", checkBrowserLang());
---
+++ WebCourseWork/js/language_pages/languages_main-page.js
@@ -0,0 +1,295 @@
+export const homePageText = {
+    "header_text": {
+        ru: "Забронировать",
+        en: "Reservation",
+    },
+    "hero_section_h1": {
+        ru: "Здоровое питание - это важно",
+        en: "Healthy Eating is important part of lifestyle",
+    },
+    "hero_section_text1": {
+        ru: "Выбор питательных веществ: улучшите свою жизнь с помощью здорового питания",
+        en: "Nutrient Choices: Fuel Your Life through Healthy Eating",
+    },
+    "hero_section_text2": {
+        ru: "Займитесь здоровьем: начните планировать свой рацион уже сегодня",
+        en: "Take control of your health: start planning your diet today",
+    },
+    "hero_section_text3": {
+        ru: "Планируйте свою диету",
+        en: "Start to plan your diet today",
+    },
+    "hero_section_text4": {
+        ru: "Отправьтесь в более здоровое завтра: начните свой путь в диете",
+        en: "Embark on a Healthier Tomorrow: Start Your Diet Journey",
+    },
+    "hero_section_text5": {
+        ru: "Вниз",
+        en: "Scroll",
+    },
+    "menu_text1": {
+        ru: "Меню",
+        en: "Our Menu",
+    },
+    "menu_text2": {
+        ru: "Это раздел нашего меню. Выберите что подходит именно вам",
+        en: "This is a section of our menu. Choose what suits you best",
+    },
+    "menu_text3": {
+        ru: "Филе глубоководной белоснежной трески",
+        en: "Deep Sea Snow White Cod Fillet",
+    },
+    "menu_text4": {
+        ru: "Побалуйте себя  — филе глубоководной трески",
+        en: "Treat yourself to a delicacy of Deep Sea Snow White Cod Fillet",
+    },
+    "menu_text5": {
+        ru: "Стейк с розмариновым маслом",
+        en: "Steak With Rosemary Butter",
+    },
+    "menu_text6": {
+        ru: "Насладитесь стейком с розмариновым маслом",
+        en: "Enjoy a Steak With Rosemary Butter",
+    },
+    "menu_text7": {
+        ru: "Салат из огурцов",
+        en: "Cucumber Salad",
+    },
+    "menu_text8": {
+        ru: "Попробуйте этот восхитительный салат из огурцов",
+        en: "Try this delicious Cucumber Salad",
+    },
+    "menu_text9": {
+        ru: "Сочетание вин",
+        en: "Natural Wine Pairing",
+    },
+    "menu_text10": {
+        ru: "Искусство натуральных сочетаний вин",
+        en: "The art of Natural Wine Pairings",
+    },
+    "chef_text1": {
+        ru: "Опытный повар",
+        en: "Excellent cook",
+    },
+    "chef_text2": {
+        ru: "Наш повар — кулинарный гений, известный своим новаторским подходом к традиционным блюдам. Он создает блюда, которые радуют чувства.",
+        en: "Our cook is a culinary genius known for his innovative approach to traditional dishes. He creates meals that delight the senses.",
+    },
+    "features_text1": {
+        ru: "Премиум качество",
+        en: "Premium Quality",
+    },
+    "features_text2": {
+        ru: "Образцовый вкус: кулинарные творения высочайшего качества",
+        en: "Exemplary Taste: elevating culinary creations with Premium Quality",
+    },
+    "features_text3": {
+        ru: "Сезонные овощи",
+        en: "Seasonal Vegetables",
+    },
+    "features_text4": {
+        ru: "Принятие свежести: изучение богатства сезонных овощей",
+        en: "Embracing Freshness: exploring the bounty of Seasonal Vegetables",
+    },
+    "features_text5": {
+        ru: "Свежие фрукты",
+        en: "Fresh Fruit",
+    },
+    "features_text6": {
+        ru: "Сладость природы: наслаждаемся прелестями свежих фруктов",
+        en: "Nature's Sweetness: savoring the delights of Fresh Fruits",
+    },
+    "post_text1": {
+        ru: "Джули Кристи",
+        en: "Julie Christie",
+    },
+    "post_post1_text2": {
+        ru: "17 октября 2021 г.",
+        en: "October 17,2021",
+    },
+    "post_post2_text2": {
+        ru: "17 октября 2021 г.",
+        en: "October 17,2021",
+    },
+    "post_post1_text3": {
+        ru: "2 комментария",
+        en: "2 comments",
+    },
+    "post_post2_text3": {
+        ru: "2 комментария",
+        en: "2 comments",
+    },
+    "post_text4": {
+        ru: "Фрукты, овощи и защита от болезней",
+        en: "Fruit and vegetables and protection against diseases",
+    },
+    "post_text5": {
+        ru: "Как фрукты и овощи защищают от болезней",
+        en: "How fruit and vegetables shield against diseases",
+    },
+    "post_post1_text6": {
+        ru: "Читать далее",
+        en: "Read More",
+    },
+    "post_post2_text6": {
+        ru: "Читать далее",
+        en: "Read More",
+    },
+    "post_text7": {
+        ru: "Дайан Рассел",
+        en: "Dianne Russell",
+    },
+    "post_text8": {
+        ru: "Салат из спаржи с рукколой и козьим сыром",
+        en: "Asparagus Spring Salad with Rocket, Goat's Cheese",
+    },
+    "post_text9": {
+        ru: "Спаржа: весенний салат с рукколой и козьим сыром",
+        en: "Using Asparagus: Spring Salad with Rocket, Goat’s Cheese",
+    },
+    "reservation_text1": {
+        ru: "Сделайте ваш заказ",
+        en: "Make a Reservation",
+    },
+    "reservation_text2": {
+        ru: "Свяжитесь с рестораном",
+        en: "Get in touch with restaurant",
+    },
+    "reservation_text3": {
+        ru: "1 Человек",
+        en: "1 Person",
+    },
+    "reservation_text4": {
+        ru: "2 Человека",
+        en: "2 Person",
+    },
+    "reservation_text5": {
+        ru: "3 Человека",
+        en: "3 Person",
+    },
+    "reservation_text6": {
+        ru: "4 Человека",
+        en: "4 Person",
+    },
+    "reservation_text7": {
+        ru: "Забронировать",
+        en: "Book Now",
+    },
+    "category_text1": {
+        ru: "Баланс калорий и энергии",
+        en: "Calories Energy Balance",
+    },
+    "category_text2": {
+        ru: "Найдите баланс калорий для вашего здоровья",
+        en: "Find Calorie Balance for Your Health",
+    },
+    "category_text3": {
+        ru: "Закуски ", 
+        en: "Starters",
+    },
+    "category_text4": {
+        ru: "Основа",
+        en: "Mains",
+    },
+    "category_text5": {
+        ru: "Супы",
+        en: "Soups",
+    },
+    "testimonial_text1": {
+        ru: "“ Мне понравилось пребывание в этом ресторане! Блюда были вкусные. Шеф-повар вкладывает свою душу в каждый рецепт.",
+        en: "“ Absolutely loved my experience here! The dishes were bursting with flavor, and you can really tell that the chef puts a lot of thought into each recipe.",
+    },
+    "testimonial_text2": {
+        ru: "“ Атмосфера была идеальной для романтического ужина. Еда была изысканной, особенно стейк с розмариновым маслом! Очень рекомендую это место.",
+        en: "“ The ambiance was perfect for a romantic dinner. The food was exquisite, especially the steak with rosemary butter! Highly recommend this place.",
+    },
+    "testimonial_text3": {
+        ru: "“ Я была здесь много раз и хочу сказать, что это место никогда не разочаровывает! Меню закусок отличное. Обязательно попробуйте блинчики с базиликом!",
+        en: "“ I’ve been here multiple times, and I want to say that it never disappoints! The starters menu is to die for. Make sure to try the basil pancakes!",
+    },
+    "testimonial_text4": {
+        ru: "“ Как восхитительно! Обслуживание было безупречным, а шеф-повар даже вышел поприветствовать нас. Видно, что они заботятся о своих клиентах.",
+        en: "“ What a delightful surprise! The service was perfect, and the chef even came out to greet us. It’s clear they care about their customers.",
+    },
+    "testimonial_text5": {
+        ru: "«Обычно я придирчива к выбору места для еды, но этот ресторан меня просто поразил! Вкусы были настолько сбалансированными, и все казалось таким свежим.",
+        en: "“ I’m usually picky about where I eat, but this restaurant blew me away! The flavors were so well-balanced, and everything felt so fresh.",
+    },
+    "testimonial_text6": {
+        ru: "“ Не могу дождаться, чтобы вернуться! Меню было фантастическим. Мне понравился креативный подход шефа. Это место обязательно нужно посетить!",
+        en: "“ I can’t wait to come back! The menu was fantastic. I loved how creative the chef got with the dishes. This place is must-visit!",
+    },
+    "testimonial_name1": {
+        ru: "Джон Доу",
+        en: "John Doe",
+    },
+    "testimonial_name2": {
+        ru: "Лидия Брэдли",
+        en: "Lydia Bradley",
+    },
+    "testimonial_name3": {
+        ru: "Грета Рэй",
+        en: "Greta Ray",
+    },
+    "testimonial_name4": {
+        ru: "Мэгги Клэй",
+        en: "Maggie Clay",
+    },
+    "testimonial_name5": {
+        ru: "Лена Стюарт",
+        en: "Lena Stuart",
+    },
+    "testimonial_name6": {
+        ru: "Нэнси Данн",
+        en: "Nancy Dunn",
+    },
+    "testimonial_client1_profession1": {
+        ru: "Блогер",
+        en: "Bloger",
+    },
+    "testimonial_client2_profession1": {
+        ru: "Блогер",
+        en: "Bloger",
+    },
+    "testimonial_client1_profession2": {
+        ru: "Фотограф",
+        en: "Photographer",
+    },
+    "testimonial_client2_profession2": {
+        ru: "Фотограф",
+        en: "Photographer",
+    },
+    "testimonial_profession3": {
+        ru: "Дизайнер",
+        en: "Designer",
+    },
+    "testimonial_profession4": {
+        ru: "Продюсер",
+        en: "Producer",
+    },
+    "footer_text1": {
+        ru: "Связь",
+        en: "Contact",
+    },
+    "footer_text2": {
+        ru: "Не пропустите рецепт",
+        en: "Never Miss a Recipe",
+    },
+    "footer_text3": {
+        ru: "Станьте подписчиком и получайте рецепты каждую неделю!",
+        en: "Join our subscribers and get best recipe delivered each week!",
+    },
+    "footer_text4": {
+        ru: "Подписка",
+        en: "Subscribe",
+    },
+    "footer_text5": {
+        ru: "© 2020 Zero Inc. Все права защищены.",
+        en: "© 2020 Zero Inc. All rights Reserved",
+    },
+    "footer_text6": {
+        ru: "Адрес элекронной почты",
+        en: "Email Address",
+    },
+
+}
