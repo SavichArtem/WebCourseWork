efa7083f74a66ade947562a6f1af779654bf1a6d
Савич Артём <200477440+SavichArtem@users.noreply.github.com>
2025-04-30 21:36:10 +0300

    fix: make navigation non-scrollable

--- WebCourseWork/js/navigation.js
+++ WebCourseWork/js/navigation.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0%"
+    document.body.style.overflow = "visible";
 }
