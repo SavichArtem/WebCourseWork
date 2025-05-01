9c0fef1f5958f2ad0d3e30c9b1ee67ae5004136b
Савич Артём <200477440+SavichArtem@users.noreply.github.com>
2025-05-01 15:56:20 +0300

    fix: make components visible on github pages

--- WebCourseWork/js/changeLanguage.js
+++ WebCourseWork/js/changeLanguage.js
@@ -74,7 +74,7 @@ function checkActiveLangButton() {
         default:
             document
                 .querySelector('[data-btn="ru"]')
-                .classList.add("header__btn_active");
+                .classList.add("header_btn_active");
             break;
     }
 }
